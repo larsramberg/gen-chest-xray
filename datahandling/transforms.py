ae5f2b6872a3d7509e4da7fb3014291bfb0c0c34
Lossar <larsivarramberg@yahoo.no>
2024-02-09 21:14:06 +0100

    Fix dependencies

--- datahandling/transforms.py
+++ datahandling/transforms.py
@@ -1,3 +1,6 @@
+from torch import nn, optim, float32, FloatTensor
+
 def to_numeric_label(in_label):
     in_label = in_label.split("|")
-    return FloatTensor([1 if (x in in_label) else 0 for x in labels])
+    return FloatTensor([1 if (x in in_label) else 0 for x in in_label])
+    
