1bb71f5d6a6b97a89ccc4b7dfc9e6caf3ed60f0e
Lossar <larsivarramberg@yahoo.no>
2024-03-05 19:19:14 +0100

    Add method to extract n amounts of labels

--- implementation/datahandling/dataloader.py
+++ implementation/datahandling/dataloader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def get_list_from_txt(path):
     '''
     Reads a .txt file and returns an array containing every line. Newline characters are stripped
@@ -7,19 +8,34 @@ def get_list_from_txt(path):
     with open(path) as f:
         return [x.rstrip() for x in f.readlines()]
 
+
 def extract_annotation_targets(annotations, filter_key, targets):
     '''
     Extracts target values from an annotations DataFrame using targets for values of the column related to the filter_key
     '''
     return annotations[annotations[filter_key].isin(targets)]
 
+
 def extract_unique_labels(annotations, label_key):
     labels = set([x for y in [x.split("|") for x in annotations[label_key]] for x in y])
     labels.remove("No Finding")
     return labels
 
+
 def extract_n_single_label_images(annotations_file, n, target_file):
     annotations = pd.read_csv(annotations_file)
     annotations = annotations[annotations["Finding Labels"].str.contains("\|") == False].tail(n)
     annotations.to_csv(target_file, index=False)
+
+
+def extract_n_images_from_labels(annotations_file, n, labels, target_file, single_only: bool):
+    result = pd.DataFrame()
+    annotations = pd.read_csv(annotations_file)
+    if single_only:
+        annotations = annotations[annotations["Finding Labels"].str.contains("\|") == False]
+    for label in labels:
+        result = pd.concat([result, annotations[annotations["Finding Labels"].str.contains(label) == True].tail(n)])
+    result.drop_duplicates()
+    result.to_csv(target_file, index=False)
+    return result
     
